1b30b02f2ae3e18fb189dfac8078eab6a8de7535
204g1a0553 <94951442+204g1a0553@users.noreply.github.com>
2022-08-30 11:55:08 +0530

    Sample.c
    V.Manohar Completed Sample.c with C source Code

---
+++ OSLAB/Sample.c
@@ -0,0 +1,11 @@
+/* Name Of the Candidate: v.Manohar
+   Roll No: 204G1A0553
+   Title of the Experiment: CPU Scheduling Using FCFS Algorithm
+   Date of Creation: 29-08-2022
+   Date of Execution: 29-08-2022
+*/
+ // Source Code
+    #include <stdio.h>
+    int main(){
+	printf("Hello World\n");
+ }
